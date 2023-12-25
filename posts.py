0575a69c0fc886d443e062af46e6019012c4c595
‪Ahmed Elbehairy‬‏ <138185767+ahmed-elbehairy7@users.noreply.github.com>
2023-12-25 17:39:10 +0000

    nothing

--- posts.py
+++ posts.py
@@ -1,24 +1,34 @@
 from os import listdir
 from json import dumps, load
-
-
-with open("posts_template.json") as file:
-    prompts = dict(load(file)['prompts'])
-
-
 import pathlib
 import textwrap
-
 import google.generativeai as genai
-
-# Used to securely store your API key
-from google.colab import userdata
-
 from IPython.display import display
 from IPython.display import Markdown
+
+
+def main():
+
+  with open("posts_template.json") as file:
+      prompts = dict(load(file)['prompts'])
+
+
+
+
+  genai.configure(api_key='AIzaSyBW8_tDl2flqdvqcxbhLDHfGNroGLetnQQ')
+
+  model = genai.GenerativeModel("gemini-pro")
+
+  response = model.generate_content("What is the meaning of life?")
+
+  print(to_markdown(response.text))
 
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
+
+
+if __name__ == "__main__":
+   main()
